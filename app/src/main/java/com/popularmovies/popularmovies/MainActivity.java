cc53816a286be06b4b7c5427dc94934ee35ac155
Safa Amin <safa2amin@gmail.com>
2018-07-08 21:28:13 +0200

    Added menu sort items

--- app/src/main/java/com/popularmovies/popularmovies/MainActivity.java
+++ app/src/main/java/com/popularmovies/popularmovies/MainActivity.java
@@ -1,7 +1,10 @@
 package com.popularmovies.popularmovies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Inflate the menu; this adds items to the action bar if it is present.
+     *
+     * @param menu The menu to inflate into
+     * @return boolean   When items inflated, return true
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    /**
+     * Handle action bar item clicks. The action bar will
+     * automatically handle clicks on the Home/Up button, so long
+     * as you specify a parent activity in AndroidManifest.xml.
+     *
+     * @param item The clicked item
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.menu_most_popular:
+                Toast.makeText(this, "Most Popular", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.menu_highest_rated:
+                Toast.makeText(this, "Highest Rated", Toast.LENGTH_LONG).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
